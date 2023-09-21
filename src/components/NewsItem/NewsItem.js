dace8fc3059aac98bcc15fd991735697510bcda6
Полухин Рашид <rashid.polukhin@gazprombank.ru>
2023-09-21 15:34:16 +0300

    Added info about news

--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -1,16 +1,16 @@
 //import React from "react";
 import style from './NewsItem.module.css'
-import {unixToDate} from "../../utils/utils";
+import {domainToHostName, unixToDate, openExternalUrl} from "../../utils/utils";
 import {Link} from 'react-router-dom'
 export function NewsItem (props){
-   const {className ='',title,date,username,score} = props
+   const {className ='',title,date,username,score,url} = props
     const scoreClassArr = [style.score]
     if (props.score>100)
-        scoreClassArr.push(style.highScore)
+    {scoreClassArr.push(style.highScore)}
     else if (props.score>30)
-        scoreClassArr.push(style.midScore)
+    { scoreClassArr.push(style.midScore)}
     else
-        scoreClassArr.push(style.lowScore)
+    {scoreClassArr.push(style.lowScore)}
 
     return (
         <div className={`${style.container} ${className}`}>
@@ -21,9 +21,16 @@ export function NewsItem (props){
                 <span>{username} | </span>
                 <span>{unixToDate(date)}</span>
             </div>
-            <div className={scoreClassArr.join(' ')}>
-                {score} points
-            </div>
+                {
+                    url ? (
+                        <div className={style.externalLink} onClick={() => openExternalUrl(url)}>{domainToHostName(url)}</div>
+                    ) :
+                        (
+                            <div className={scoreClassArr.join(' ')}>
+                                {score} points
+                            </div>
+                        )
+                }
             </div>
         </div>
     )
